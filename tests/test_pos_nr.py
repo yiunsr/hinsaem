de4301c269fa962736977f112bf5f6629e58228d
yiunsr <yiunsr@gmail.com>
2018-03-21 20:35:34 +0900

    수사 testcase 추가

--- tests/test_pos_nr.py
+++ tests/test_pos_nr.py
@@ -33,6 +33,11 @@ def test_0002_nr():
     assert posNR.check(u"스물하나"), u"스물하나 is nr"
     assert posNR.check(u"마흔둘"), u"마흔둘 is nr"
     assert posNR.check(u"백만스물하나"), u"백만스물하나 is nr"
+    
+    """ 고유어 수사 검사 2"""
+    assert posNR.check(u"한두"), u"한두 is nr"
+    assert posNR.check(u"예닐곱"), u"예닐곱 is nr"
+    
 
 def test_0003_nr():
     """ 한자어 수사 검사 """
@@ -41,7 +46,11 @@ def test_0003_nr():
     assert posNR.check(u"일백만"), u"일백만 is nr"
     assert posNR.check(u"이천구백삼십일"), u"이천구백삼십일 is nr"
     assert posNR.check(u"이십만이천"), u"이십만이천 is nr"
-
+    
+    """ 접두어가 붙은 수사 """
+    assert posNR.check(u"몇십"), u"몇십 is nr"
+    assert posNR.check(u"기백"), u"기백 is nr"
+    assert posNR.check(u"수천만"), u"수천만 is nr"
 
 if __name__ == "__main__":
     pytest.main([__file__])
